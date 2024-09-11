c9d4fb87cf96957c7ed1c8a5eb6cd2bc66fcac12
Li Huang <lihuang.dmft@gmail.com>
2024-09-11 15:42:39 +0800

    fix util.jl

--- src/util.jl
+++ src/util.jl
@@ -4,5 +4,258 @@
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2024/09/10
+# Last modified: 2024/09/11
 #
+
+"""
+    sorry()
+
+Print an error message to the screen.
+
+### Arguments
+N/A
+
+### Returns
+N/A
+"""
+function sorry()
+    error("Sorry, this feature has not been implemented")
+end
+
+"""
+    prompt(msg::String)
+
+Print a stylized ACFlow message to the screen.
+
+### Arguments
+* msg -> Message that need to be printed.
+
+### Returns
+N/A
+"""
+function prompt(msg::String)
+    print(green("ACFlow > "))
+    print(magenta(msg))
+    println()
+    #
+    flush(stdout)
+end
+
+#=
+### *I/O Operations*
+=#
+
+"""
+    line_to_array(io::IOStream)
+
+Convert a line (reading from an IOStream) to a string array.
+
+### Arguments
+* io -> An IOStream struct.
+
+### Returns
+* arr -> An array  of String.
+"""
+@inline function line_to_array(io::IOStream)
+    split(readline(io), " ", keepempty = false)
+end
+
+"""
+    line_to_array(str::AbstractString)
+
+Convert a string (AbstractString) to a string array.
+
+### Arguments
+* str -> A String.
+
+### Returns
+* ass -> An array of String.
+
+### Examples
+```julia-repl
+julia> str = "Hello World!"
+"Hello World!"
+
+julia> line_to_array(str)
+2-element Vector{SubString{String}}:
+ "Hello"
+ "World!"
+```
+"""
+@inline function line_to_array(str::AbstractString)
+    split(str, " ", keepempty = false)
+end
+
+#=
+### *Color Tools*
+=#
+
+#=
+*Remarks* :
+
+The purpose of the following codes is to provide some convenient tools
+to output colorful and stylized texts in the terminal. Actually, these
+codes are inspried by this repository:
+
+* https://github.com/Aerlinger/AnsiColor.jl
+
+For more information about the ANSI color escape sequences, please check
+the following websites further:
+
+* https://stackoverflow.com/questions/4842424/
+* https://en.wikipedia.org/wiki/ANSI_escape_code
+
+Note that the macro `@pcs` and functions `prompt()` rely on these codes.
+=#
+
+"""
+    COLORS
+
+A global dict, which is used to specify the system colors.
+"""
+const COLORS = Dict{String,I64}(
+    "black"          => 0,
+    "red"            => 1,
+    "green"          => 2,
+    "yellow"         => 3,
+    "blue"           => 4,
+    "magenta"        => 5,
+    "cyan"           => 6,
+    "white"          => 7,
+    "default"        => 9,
+    "light_black"    => 60,
+    "light_red"      => 61,
+    "light_green"    => 62,
+    "light_yellow"   => 63,
+    "light_blue"     => 64,
+    "light_magenta"  => 65,
+    "light_cyan"     => 66,
+    "light_white"    => 67
+)
+
+"""
+    MODES
+
+A global dict, which is used to specify the mode for output characters.
+"""
+const MODES = Dict{String,I64}(
+    "default"        => 0,
+    "bold"           => 1,
+    "underline"      => 4,
+    "blink"          => 5,
+    "swap"           => 7,
+    "hide"           => 8
+)
+
+"""
+    colorize(
+        c::String,
+        s::String;
+        bg::String = "default",
+        m::String = "default"
+    )
+
+Return some escape sequences, which will be displayed as colorized texts
+in the terminal.
+
+### Arguments
+* c  -> Color names.
+* s  -> The string that want to be printed.
+* bg -> Background color.
+* m  -> Output mode.
+
+### Returns
+* See above explanations.
+"""
+function colorize(
+    c::String,
+    s::String;
+    bg::String = "default",
+    m::String = "default"
+    )
+    C_OFFSET = 30
+    B_OFFSET = 40
+    "\033[$(MODES[m]);$(C_OFFSET + COLORS[c]);$(B_OFFSET + COLORS[bg])m$(s)\033[0m"
+end
+
+"""
+    colorize(
+        c::Symbol,
+        s::String;
+        bg::String = "default",
+        m::String = "default"
+    )
+
+Return some escape sequences, which will be displayed as colorized texts
+in the terminal.
+
+### Arguments
+* c  -> Color names.
+* s  -> The string that want to be printed.
+* bg -> Background color.
+* m  -> Output mode.
+
+### Returns
+* See above explanations.
+"""
+function colorize(
+    c::Symbol,
+    s::String;
+    bg::String = "default",
+    m::String = "default"
+    )
+    colorize(string(c), s; bg=bg, m=m)
+end
+
+#=
+*Remarks* :
+
+The following codes will generate and export dynamically some color
+functions, including:
+
+```julia
+# For standard colors
+black(str::String)
+red(str::String)
+green(str::String)
+yellow(str::String)
+blue(str::String)
+magenta(str::String)
+cyan(str::String)
+white(str::String)
+```
+
+and their light color versions
+
+```julia
+# For light colors
+light_black(str::String)
+light_red(str::String)
+light_green(str::String)
+light_yellow(str::String)
+light_blue(str::String)
+light_magenta(str::String)
+light_cyan(str::String)
+light_white(str::String)
+```
+
+These functions provide some shortcuts to create texts decorated by
+special escape sequences. These texts will be show as colorized texts
+in the terminal.
+
+### Examples
+```julia-repl
+julia> println(red("hello world!"))
+```
+=#
+
+export COLORS
+export MODES
+export colorize
+
+for k in keys(COLORS)
+    f = Symbol(k)
+    k == "default" && continue
+    @eval ($f)(str::String) = colorize(Symbol($f), str)
+    @eval export ($f)
+end
