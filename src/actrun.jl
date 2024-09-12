6b25959a6c0ccee65c999f2235127a86e21ac6d7
Li Huang <lihuang.dmft@gmail.com>
2024-09-12 19:57:53 +0800

    fix actrun.jl

--- src/actrun.jl
+++ src/actrun.jl
@@ -1,3 +1,14 @@
+#!/usr/bin/env julia
+  
+#
+# This script is used to start analytic continuation simulations.
+# It will launch only 1 process.
+#
+# Usage:
+#
+#     $ actrun.jl act.toml
+#
+
 push!(LOAD_PATH, "../src")
 
 using ACTest
