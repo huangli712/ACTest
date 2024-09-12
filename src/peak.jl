a299bb22f6b17decd84b2f2c60bcfc9f7a71229d
Li Huang <lihuang.dmft@gmail.com>
2024-09-12 22:17:32 +0800

    fix peak.jl

--- src/peak.jl
+++ src/peak.jl
@@ -4,14 +4,15 @@
 # Author  : Li Huang (huangli@caep.cn)
 # Status  : Unstable
 #
-# Last modified: 2024/09/11
+# Last modified: 2024/09/12
 #
 
-function GaussianPeak()
+function (𝑝::GaussianPeak)(ω::F64)
+    return 𝑝.A * exp( -(ω - 𝑝.ϵ) ^ 2.0 / (2.0 * 𝑝.Γ ^ 2.0) )
 end
 
-function LorentzianPeak()
+function (𝑝::LorentzianPeak)(ω::F64)
 end
 
-function RectanglePeak()
+function (𝑝::RectanglePeak)(ω::F64)
 end
