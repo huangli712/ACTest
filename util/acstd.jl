9518bc5cee757af406082781e3eed0f90f2f4ebb
Li Huang <lihuang.dmft@gmail.com>
2024-09-18 00:42:00 +0800

    add acstd.jl

---
+++ util/acstd.jl
@@ -0,0 +1,20 @@
+#!/usr/bin/env julia
+  
+#
+# This script is used to generate spectral functions and corresponding
+# correlation functions. It will launch only 1 process.
+#
+# Usage:
+#
+#     $ acgen.jl act.toml
+#
+
+push!(LOAD_PATH, "/Users/lihuang/Working/devel/ACTest/src")
+
+using ACTest
+
+welcome()
+overview()
+read_param()
+make_data()
+goodbye()
