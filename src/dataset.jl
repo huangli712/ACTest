805e00a0830a5ae4c1da4f75660174d4b82ba1c9
Li Huang <lihuang.dmft@gmail.com>
2024-09-18 11:30:26 +0800

    fix dataset.jl

--- src/dataset.jl
+++ src/dataset.jl
@@ -277,269 +277,269 @@ const STD_FG = Dict{String,Any}[
 ]
 
 const STD_FD = Dict{String,Any}[
-    # Test: 001 / Fermionic + Gaussian Peaks
+    # Test: 001 / Fermionic + Delta Peaks
     # single peak, central
     Dict(
         "ktype" => "fermi",
         "grid"  => "ffreq",
         "mesh"  => "linear",
         "peaks" => [
-            GaussianPeak(1.0,0.8,0.0)
+            DeltaPeak(1.0,0.8,0.0)
         ],
         "signs" => [1.0]
     ),
     #
-    # Test: 002 / Fermionic + Gaussian Peaks
+    # Test: 002 / Fermionic + Delta Peaks
     # single peak, off-centered (left)
     Dict(
         "ktype" => "fermi",
         "grid"  => "ffreq",
         "mesh"  => "linear",
         "peaks" => [
-            GaussianPeak(1.0,0.8,-2.0)
+            DeltaPeak(1.0,0.8,-2.0)
         ],
         "signs" => [1.0]
     ),
     #
-    # Test: 003 / Fermionic + Gaussian Peaks
+    # Test: 003 / Fermionic + Delta Peaks
     # single peak, off-centered (right)
     Dict(
         "ktype" => "fermi",
         "grid"  => "ffreq",
         "mesh"  => "linear",
         "peaks" => [
-            GaussianPeak(1.0,0.8,2.0)
+            DeltaPeak(1.0,0.8,2.0)
         ],
         "signs" => [1.0]
     ),
     #
-    # Test: 004 / Fermionic + Gaussian Peaks
+    # Test: 004 / Fermionic + Delta Peaks
     # two peaks, gapless
     Dict(
         "ktype" => "fermi",
         "grid"  => "ffreq",
         "mesh"  => "linear",
         "peaks" => [
-            GaussianPeak(1.0,0.5,-0.8),
-            GaussianPeak(1.0,0.5,0.8)
+            DeltaPeak(1.0,0.5,-0.8),
+            DeltaPeak(1.0,0.5,0.8)
         ],
         "signs" => [1.0,1.0]
     ),
     #
-    # Test: 005 / Fermionic + Gaussian Peaks
+    # Test: 005 / Fermionic + Delta Peaks
     # two peaks, small gap
     Dict(
         "ktype" => "fermi",
         "grid"  => "ffreq",
         "mesh"  => "linear",
         "peaks" => [
-            GaussianPeak(1.0,0.5,-1.8),
-            GaussianPeak(1.0,0.5,1.8)
+            DeltaPeak(1.0,0.5,-1.8),
+            DeltaPeak(1.0,0.5,1.8)
         ],
         "signs" => [1.0,1.0]
     ),
     #
-    # Test: 006 / Fermionic + Gaussian Peaks
+    # Test: 006 / Fermionic + Delta Peaks
     # two peaks, large gap
     Dict(
         "ktype" => "fermi",
         "grid"  => "ffreq",
         "mesh"  => "linear",
         "peaks" => [
-            GaussianPeak(1.0,0.5,-3.0),
-            GaussianPeak(1.0,0.5,3.0)
+            DeltaPeak(1.0,0.5,-3.0),
+            DeltaPeak(1.0,0.5,3.0)
         ],
         "signs" => [1.0,1.0]
     ),
     #
-    # Test: 007 / Fermionic + Gaussian Peaks
+    # Test: 007 / Fermionic + Delta Peaks
     # two peaks, central + off-centered (left)
     Dict(
         "ktype" => "fermi",
         "grid"  => "ffreq",
         "mesh"  => "linear",
         "peaks" => [
-            GaussianPeak(1.0,0.4,0.0),
-            GaussianPeak(1.0,0.4,-3.0)
+            DeltaPeak(1.0,0.4,0.0),
+            DeltaPeak(1.0,0.4,-3.0)
         ],
         "signs" => [1.0,1.0]
     ),
     #
-    # Test: 008 / Fermionic + Gaussian Peaks
+    # Test: 008 / Fermionic + Delta Peaks
     # two peaks, central + off-centered (right)
     Dict(
         "ktype" => "fermi",
         "grid"  => "ffreq",
         "mesh"  => "linear",
         "peaks" => [
-            GaussianPeak(1.0,0.4,0.0),
-            GaussianPeak(1.0,0.4,3.0)
+            DeltaPeak(1.0,0.4,0.0),
+            DeltaPeak(1.0,0.4,3.0)
         ],
         "signs" => [1.0,1.0]
     ),
     #
-    # Test: 009 / Fermionic + Gaussian Peaks
+    # Test: 009 / Fermionic + Delta Peaks
     # two peaks, off-centered (left small + right big)
     Dict(
         "ktype" => "fermi",
         "grid"  => "ffreq",
         "mesh"  => "linear",
         "peaks" => [
-            GaussianPeak(0.5,0.5,-3.0),
-            GaussianPeak(1.5,0.5,3.0)
+            DeltaPeak(0.5,0.5,-3.0),
+            DeltaPeak(1.5,0.5,3.0)
         ],
         "signs" => [1.0,1.0]
     ),
     #
-    # Test: 010 / Fermionic + Gaussian Peaks
+    # Test: 010 / Fermionic + Delta Peaks
     # two peaks, off-centered (left big + right small)
     Dict(
         "ktype" => "fermi",
         "grid"  => "ffreq",
         "mesh"  => "linear",
         "peaks" => [
-            GaussianPeak(1.5,0.5,-3.0),
-            GaussianPeak(0.5,0.5,3.0)
+            DeltaPeak(1.5,0.5,-3.0),
+            DeltaPeak(0.5,0.5,3.0)
         ],
         "signs" => [1.0,1.0]
     ),
     #
-    # Test: 011 / Fermionic + Gaussian Peaks
+    # Test: 011 / Fermionic + Delta Peaks
     # three peaks, gapless
     Dict(
         "ktype" => "fermi",
         "grid"  => "ffreq",
         "mesh"  => "linear",
         "peaks" => [
-            GaussianPeak(1.0,0.1,0.0),
-            GaussianPeak(0.5,1.0,-1.0),
-            GaussianPeak(0.5,1.0,1.0)
+            DeltaPeak(1.0,0.1,0.0),
+            DeltaPeak(0.5,1.0,-1.0),
+            DeltaPeak(0.5,1.0,1.0)
         ],
         "signs" => [1.0,1.0,1.0]
     ),
     #
-    # Test: 012 / Fermionic + Gaussian Peaks
+    # Test: 012 / Fermionic + Delta Peaks
     # three peaks, gapless
     Dict(
         "ktype" => "fermi",
         "grid"  => "ffreq",
         "mesh"  => "linear",
         "peaks" => [
-            GaussianPeak(1.0,0.1,0.0),
-            GaussianPeak(0.5,1.0,-2.0),
-            GaussianPeak(0.5,1.0,2.0)
+            DeltaPeak(1.0,0.1,0.0),
+            DeltaPeak(0.5,1.0,-2.0),
+            DeltaPeak(0.5,1.0,2.0)
         ],
         "signs" => [1.0,1.0,1.0]
     ),
     #
-    # Test: 013 / Fermionic + Gaussian Peaks
+    # Test: 013 / Fermionic + Delta Peaks
     # three peaks, gapless
     Dict(
         "ktype" => "fermi",
         "grid"  => "ffreq",
         "mesh"  => "linear",
         "peaks" => [
-            GaussianPeak(1.0,0.05,0.0),
-            GaussianPeak(0.5,1.0,-3.0),
-            GaussianPeak(0.5,1.0,3.0)
+            DeltaPeak(1.0,0.05,0.0),
+            DeltaPeak(0.5,1.0,-3.0),
+            DeltaPeak(0.5,1.0,3.0)
         ],
         "signs" => [1.0,1.0,1.0]
     ),
     #
-    # Test: 014 / Fermionic + Gaussian Peaks
+    # Test: 014 / Fermionic + Delta Peaks
     # three peaks, gapless
     Dict(
         "ktype" => "fermi",
         "grid"  => "ffreq",
         "mesh"  => "linear",
         "peaks" => [
-            GaussianPeak(1.0,0.05,0.0),
-            GaussianPeak(0.4,0.5,-3.5),
-            GaussianPeak(0.4,0.5,3.5)
+            DeltaPeak(1.0,0.05,0.0),
+            DeltaPeak(0.4,0.5,-3.5),
+            DeltaPeak(0.4,0.5,3.5)
         ],
         "signs" => [1.0,1.0,1.0]
     ),
     #
-    # Test: 015 / Fermionic + Gaussian Peaks
+    # Test: 015 / Fermionic + Delta Peaks
     # three peaks, large gap
     Dict(
         "ktype" => "fermi",
         "grid"  => "ffreq",
         "mesh"  => "linear",
         "peaks" => [
-            GaussianPeak(1.0,0.05,-1.0),
-            GaussianPeak(0.4,0.8,-3.0),
-            GaussianPeak(0.4,0.8,3.0)
+            DeltaPeak(1.0,0.05,-1.0),
+            DeltaPeak(0.4,0.8,-3.0),
+            DeltaPeak(0.4,0.8,3.0)
         ],
         "signs" => [1.0,1.0,1.0]
     ),
     #
-    # Test: 016 / Fermionic + Gaussian Peaks
+    # Test: 016 / Fermionic + Delta Peaks
     # three peaks, large gap
     Dict(
         "ktype" => "fermi",
         "grid"  => "ffreq",
         "mesh"  => "linear",
         "peaks" => [
-            GaussianPeak(1.0,0.05,1.0),
-            GaussianPeak(0.4,0.8,-3.0),
-            GaussianPeak(0.4,0.8,3.0)
+            DeltaPeak(1.0,0.05,1.0),
+            DeltaPeak(0.4,0.8,-3.0),
+            DeltaPeak(0.4,0.8,3.0)
         ],
         "signs" => [1.0,1.0,1.0]
     ),
     #
-    # Test: 017 / Fermionic + Gaussian Peaks
+    # Test: 017 / Fermionic + Delta Peaks
     # three peaks, gapless, left
     Dict(
         "ktype" => "fermi",
         "grid"  => "ffreq",
         "mesh"  => "linear",
         "peaks" => [
-            GaussianPeak(1.0,0.2,0.0),
-            GaussianPeak(0.6,0.6,-1.0),
-            GaussianPeak(0.6,0.6,-3.0)
+            DeltaPeak(1.0,0.2,0.0),
+            DeltaPeak(0.6,0.6,-1.0),
+            DeltaPeak(0.6,0.6,-3.0)
         ],
         "signs" => [1.0,1.0,1.0]
     ),
     #
-    # Test: 018 / Fermionic + Gaussian Peaks
+    # Test: 018 / Fermionic + Delta Peaks
     # three peaks, gapless, right
     Dict(
         "ktype" => "fermi",
         "grid"  => "ffreq",
         "mesh"  => "linear",
         "peaks" => [
-            GaussianPeak(1.0,0.2,0.0),
-            GaussianPeak(0.6,0.6,1.0),
-            GaussianPeak(0.6,0.6,3.0)
+            DeltaPeak(1.0,0.2,0.0),
+            DeltaPeak(0.6,0.6,1.0),
+            DeltaPeak(0.6,0.6,3.0)
         ],
         "signs" => [1.0,1.0,1.0]
     ),
     #
-    # Test: 019 / Fermionic + Gaussian Peaks
+    # Test: 019 / Fermionic + Delta Peaks
     # three peaks, gapless, left small, right big
     Dict(
         "ktype" => "fermi",
         "grid"  => "ffreq",
         "mesh"  => "linear",
         "peaks" => [
-            GaussianPeak(1.0,0.2,0.0),
-            GaussianPeak(0.1,0.4,-2.0),
-            GaussianPeak(0.6,1.0,2.0)
+            DeltaPeak(1.0,0.2,0.0),
+            DeltaPeak(0.1,0.4,-2.0),
+            DeltaPeak(0.6,1.0,2.0)
         ],
         "signs" => [1.0,1.0,1.0]
     ),
     #
-    # Test: 020 / Fermionic + Gaussian Peaks
+    # Test: 020 / Fermionic + Delta Peaks
     # three peaks, gapless, left big, right small
     Dict(
         "ktype" => "fermi",
         "grid"  => "ffreq",
         "mesh"  => "linear",
         "peaks" => [
-            GaussianPeak(1.0,0.2,0.0),
-            GaussianPeak(0.1,0.4,2.0),
-            GaussianPeak(0.6,1.0,-2.0)
+            DeltaPeak(1.0,0.2,0.0),
+            DeltaPeak(0.1,0.4,2.0),
+            DeltaPeak(0.6,1.0,-2.0)
         ],
         "signs" => [1.0,1.0,1.0]
     ),
