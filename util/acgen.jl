a7275c439a196346d431b16cbab2d85d469b10fb
Li Huang <lihuang.dmft@gmail.com>
2024-09-18 19:26:53 +0800

    fix acgen.jl

--- util/acgen.jl
+++ util/acgen.jl
@@ -1,15 +1,13 @@
 #!/usr/bin/env julia
   
 #
-# This script is used to generate spectral functions and corresponding
+# This script is used to build the spectral functions and the related
 # correlation functions. It will launch only 1 process.
 #
 # Usage:
 #
 #     $ acgen.jl act.toml
 #
-
-push!(LOAD_PATH, "/Users/lihuang/Working/devel/ACTest/src")
 
 using ACTest
 
