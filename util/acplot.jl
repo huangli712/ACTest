7e9901bfbaee06c008b4dbc82058c1679eea7262
Li Huang <lihuang.dmft@gmail.com>
2024-09-19 10:45:34 +0800

    fix acplot.jl

--- util/acplot.jl
+++ util/acplot.jl
@@ -12,23 +12,48 @@
 #
 #     $ acplot.jl act.toml
 #
+push!(LOAD_PATH, "/Users/lihuang/Working/devel/ACTest/src/")
 
-using CairoMakie
 using ACTest
 
-function read_Aout(ind::I64)
+using CairoMakie
+using Printf
 
+function read_Aout(ind::I64)
+    println("1Aout")
+    error("Aout")
 end
 
 function read_image(ind::I64)
-
+    error("image")
 end
 
 function make_plot(ind::I64, sf1::SpectralFunction, sf2::SpectralFunction)
-    
+    error("plot")
+end
+
+function make_figures()
+    ntest = get_t("ntest")
+    @show ntest
+
+    # Start the loop
+    for i = 1:ntest
+        @printf("Test -> %4i / %4i\n", i, ntest)
+        #
+        try
+            sf1 = read_image(i)
+            sf2 = read_Aout(i)
+            make_plot(i, sf1, sf2)        
+        catch ex
+            println("Something wrong for test case $i")
+        end
+        #
+        println()
+    end
 end
 
 welcome()
 overview()
 read_param()
+make_figures()
 goodbye()
