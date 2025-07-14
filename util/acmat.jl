f50e054dd321e4aef31957809fead07b7043e526
Li Huang <lihuang.dmft@gmail.com>
2025-07-14 23:31:58 +0800

    add acmat.jl

--- util/acmat.jl
+++ util/acmat.jl
@@ -1,5 +1,17 @@
 #!/usr/bin/env julia
 
+#
+# This script is used to create a standard dataset, which contains 100
+# spectral functions and related correlation functions. This dataset
+# is called ACT100. It should be used to benchmark the newly developed
+# analytic continuation tools and methods. This script will launch only
+# 1 process.
+#
+# Usage:
+#
+#     $ ./acmat.jl act.toml
+#
+
 haskey(ENV,"ACTEST_HOME") && pushfirst!(LOAD_PATH, ENV["ACTEST_HOME"])
 
 using ACTest
@@ -7,3 +19,4 @@ using ACTest
 welcome()
 overview()
 read_param()
+goodbye()
